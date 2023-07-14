62947cfcf01a2c33bd03f5336c7b86302e06bd41
David Segura <david@segura.org>
2023-07-14 14:25:15 -0500

    Create HighPerformance.ps1

---
+++ Power/HighPerformance.ps1
@@ -0,0 +1,5 @@
+# Create Ultimate Performance Power Plan
+powercfg -DuplicateScheme e9a42b02-d5df-448d-aa00-03f14749eb61
+powercfg -SetActive 80f744fe-9c02-4a5e-8ea2-cfee24d3cffc
+
+#381b4222-f694-41f0-9685-ff5bb260df2e
