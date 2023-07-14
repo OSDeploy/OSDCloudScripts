a9f69d56f24b46409f42a72e1c2e9062c2d95a91
David Segura <david@segura.org>
2023-07-14 16:56:10 -0500

    Update Power Plans

---
+++ Power/Set-HPOptimizedModernStandby.ps1
@@ -0,0 +1,2 @@
+# This power plan may not exist on all computers
+powercfg /setactive fb5220ff-7e1a-47aa-9a42-50ffbf45c673
--- Power/Set-UltimatePerformance.ps1
+++ Power/Set-UltimatePerformance.ps1
@@ -1,1 +1,2 @@
+# This may not exist on all computers
 powercfg /setactive 80f744fe-9c02-4a5e-8ea2-cfee24d3cffc
