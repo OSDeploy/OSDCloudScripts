2cbbdc82c4650abbe508cdba1eec7f320778da58
David Segura <david@segura.org>
2023-07-16 16:27:08 -0500

    Update Clear-LocalDisk.ps1

--- WinPE/Clear-LocalDisk.ps1
+++ WinPE/Clear-LocalDisk.ps1
@@ -1,23 +1,8 @@
-<#PSScriptInfo
-.VERSION 23.6.1.2
-.GUID 874c7a14-b050-4950-a1ef-cb70a0d60061
-.AUTHOR David Segura
-.COMPANYNAME David Segura
-.COPYRIGHT (c) 2023 David Segura. All rights reserved.
-.TAGS WinPE
-.LICENSEURI 
-.PROJECTURI https://github.com/OSDeploy/PwshHub
-.ICONURI 
-.EXTERNALMODULEDEPENDENCIES 
-.REQUIREDSCRIPTS 
-.EXTERNALSCRIPTDEPENDENCIES 
-.RELEASENOTES
-#>
 #Requires -Modules @{ ModuleName="OSD"; ModuleVersion="23.5.26.1" }
 #Requires -RunAsAdministrator
 <#
 .DESCRIPTION
-Clears the Local Disk
+Clears the Local Disk using the Clear-LocalDisk function in the OSD Module
 #>
 [CmdletBinding()]
 param()
