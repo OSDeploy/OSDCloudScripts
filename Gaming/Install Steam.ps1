30e2bae768758869bca4d716eb26e3361e6cd286
David Segura <david@segura.org>
2023-07-20 13:06:04 -0500

    Update Install Steam.ps1

---
+++ Gaming/Install Steam.ps1
@@ -0,0 +1,24 @@
+#Requires -RunAsAdministrator
+<#
+.DESCRIPTION
+Install Package using WinGet
+#>
+[CmdletBinding()]
+param(
+    [ValidateNotNullOrEmpty()]
+    [string]$id = 'Valve.Steam'
+)
+
+if (Get-Command 'WinGet' -ErrorAction SilentlyContinue) {
+    # Show package information
+    # winget show --id $id
+    
+    # Show version information
+    # winget show --id $id --versions
+    
+    # Install
+    winget install --id $id --exact --accept-source-agreements --accept-package-agreements
+}
+else {
+    Write-Error -Message 'WinGet is not installed.'
+}
