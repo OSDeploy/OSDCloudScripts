970e31c11ac7ce305a8464ed439e9e9104c3823a
JM2K69 <bezettorres.jerome@neuf.fr>
2023-07-23 11:12:02 +0200

    23.7.23.1
    Fix web link for Install powerShell

--- Software/Install-PowerShell7.ps1
+++ Software/Install-PowerShell7.ps1
@@ -1,4 +1,5 @@
-
-https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.3
+<#
+    https://learn.microsoft.com/en-us/powershell/scripting/install/installing-powershell-on-windows?view=powershell-7.3
+#>
 
 Invoke-Expression "& { $(Invoke-RestMethod https://aka.ms/install-powershell.ps1) } -UseMSI"
