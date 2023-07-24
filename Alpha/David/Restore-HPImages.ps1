60e1a50a3fae10df6855223c40e326b086e3c941
David Segura <david@segura.org>
2023-07-24 16:04:41 -0500

    David Update

--- Alpha/David/Restore-HPImages.ps1
+++ Alpha/David/Restore-HPImages.ps1
@@ -4,6 +4,9 @@ This is a script that will recreate the partition structure that came on an HP E
 .LINK
 https://learn.microsoft.com/en-us/windows-hardware/manufacture/desktop/oem-deployment-of-windows-desktop-editions-sample-scripts?preserve-view=true&view=windows-10#-createpartitions-uefitxt
 #>
+$ImageRoot = 'E:\OSDCloud\OS\HP\EliteBook860-5CG3270RZK'
+
+
 $DiskNumber = 0
 
 #Partition 1
@@ -16,20 +19,11 @@ $EfiLabel = 'System'
 $WindowsLabel = 'Windows'
 $WindowsDriveLetter = 'C'
 
-$ShrinkSize = 33750
+$ShrinkSize = 982
 
 #Partition 4
 $Partition4Size = 982
 $Partition4Label = 'Windows RE'
-
-#Partition 5 FAT32
-$Partition5Size = 1024
-$Partition5Label = 'SR_AED'
-
-#Partition 6 is the Recovery Image
-$RecoveryImageSize = 31744
-$RecoveryImageLabel = 'SR_IMAGE'
-
 
 $DiskpartScript = @"
 rem == CreatePartitions-UEFI.txt ==
@@ -66,14 +60,6 @@ format quick fs=ntfs label="$Partition4Label"
 assign letter="R"
 set id="de94bba4-06d1-4d40-a16a-bfd50179d6ac"
 gpt attributes=0x8000000000000001
-rem === 5. FAT32 partition ======================
-create partition primary size=$Partition5Size
-format quick fs=fat32 label="$Partition5Label"
-assign letter="T"
-rem === 6. Recovery Image partition ======================
-create partition primary
-format quick fs=ntfs label="$RecoveryImageLabel"
-assign letter="U"
 list volume
 exit
 "@
@@ -81,6 +67,13 @@ exit
 if ($env:SystemDrive -eq 'X:') {
     $DiskpartScript | Out-File X:\CreatePartitions-UEFI.txt -Encoding ASCII
     DiskPart /s X:\CreatePartitions-UEFI.txt
+
+    if (Test-Path "$ImageRoot\1-SYSTEM.wim") {
+        Expand-WindowsImage -ApplyPath S:\ -ImagePath "$ImageRoot\1-SYSTEM.wim" -Index 1
+        Expand-WindowsImage -ApplyPath R:\ -ImagePath "$ImageRoot\3-Windows.wim" -Index 1
+        Expand-WindowsImage -ApplyPath C:\ -ImagePath "$ImageRoot\4-Windows RE Tools.wim" -Index 1
+        #C:\Windows\System32\bcdboot.exe C:\Windows /v /p
+    }
 }
 else {
     Write-Warning "This script must be run in WinPE"
---
+++ Reagent/New-ReAgentXml.ps1
@@ -0,0 +1,29 @@
+$ReAgentXmlPath = 'C:\Windows\System32\Recovery\ReAgent.xml'
+
+$ReAgentXml = @'
+<?xml version='1.0' encoding='utf-8'?>
+
+<WindowsRE version="2.0">
+  <WinreBCD id="{00000000-0000-0000-0000-000000000000}"/>
+  <WinreLocation path="" id="0" offset="0" guid="{00000000-0000-0000-0000-000000000000}"/>
+  <ImageLocation path="" id="0" offset="0" guid="{00000000-0000-0000-0000-000000000000}"/>
+  <PBRImageLocation path="" id="0" offset="0" guid="{00000000-0000-0000-0000-000000000000}" index="0"/>
+  <PBRCustomImageLocation path="" id="0" offset="0" guid="{00000000-0000-0000-0000-000000000000}" index="0"/>
+  <InstallState state="0"/>
+  <OsInstallAvailable state="0"/>
+  <CustomImageAvailable state="0"/>
+  <IsAutoRepairOn state="0"/>
+  <WinREStaged state="0"/>
+  <OperationParam path=""/>
+  <OperationPermanent state="0"/>
+  <OsBuildVersion path=""/>
+  <OemTool state="0"/>
+  <IsServer state="0"/>
+  <DownlevelWinreLocation path="" id="0" offset="0" guid="{00000000-0000-0000-0000-000000000000}"/>
+  <IsWimBoot state="0"/>
+  <NarratorScheduled state="0"/>
+  <ScheduledOperation state="0"/>
+</WindowsRE>
+'@
+
+$ReAgentXml | Out-File -FilePath $ReAgentXmlPath -Encoding UTF8 -Force
