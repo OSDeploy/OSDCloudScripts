98ff6e58f36583905671668a6f022123dfb430e0
JM2K69 <bezettorres.jerome@neuf.fr>
2023-07-24 10:44:25 +0200

    23.7.24.1

---
+++ Software/Live-Sysinternals.ps1
@@ -0,0 +1,18 @@
+<#PSScriptInfo
+.VERSION 23.7.24.1
+.GUID 3119fc78-1038-4785-8a59-5730423f3732
+.AUTHOR Bezet-Torres Jérôme
+.COMPANYNAME Bezet-Torres Jérôme
+.COPYRIGHT (c) 2023 Bezet-Torres Jérôme. All rights reserved.
+.TAGS DEV PARAM
+.LICENSEURI 
+.PROJECTURI https://github.com/OSDeploy/OSDCloudScripts
+.ICONURI 
+.EXTERNALMODULEDEPENDENCIES 
+.REQUIREDSCRIPTS 
+.EXTERNALSCRIPTDEPENDENCIES 
+.RELEASENOTES
+#>
+
+net use Z: http://live.sysinternals.com/tools
+
