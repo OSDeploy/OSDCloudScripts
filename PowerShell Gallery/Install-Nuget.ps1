0df9923233c6db685d92d315609b251bf1d6d205
David Segura <david@segura.org>
2023-08-09 12:08:50 -0500

    Update Install-Nuget.ps1

--- PowerShell Gallery/Install-Nuget.ps1
+++ PowerShell Gallery/Install-Nuget.ps1
@@ -1,1 +1,3 @@
 Install-PackageProvider -Name NuGet -MinimumVersion 2.8.5.201 -Force
+
+#Invoke-WebRequest -Uri https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile "$env:LOCALAPPDATA\Microsoft\Windows\PowerShell\PowerShellGet\NuGet.exe"
