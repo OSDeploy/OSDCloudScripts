efc50fbe2dc69fc354aa91e7f127a145b2ce84ab
David Segura <david@segura.org>
2023-08-09 08:36:22 -0500

    Create Test-HpBiosBatteryHealthManager.ps1

---
+++ HP/BIOS/BatteryHealthManager/Test-HpBiosBatteryHealthManager.ps1
@@ -0,0 +1,15 @@
+﻿# https://h20195.www2.hp.com/v2/GetDocument.aspx?docname=4AA7-8536ENW
+
+$Namespace = 'root\HP\InstrumentedBIOS'
+$Class = 'HP_BIOSSetting'
+$ClassInterface = 'HP_BIOSSettingInterface'
+
+$Name = 'Battery Health Manager'
+
+try {
+    $Result = Get-WmiObject -Namespace $Namespace -Class $Class -Filter "name='$Name'" -ErrorAction Stop
+    Return $true
+}
+catch {
+    Return $false
+}
