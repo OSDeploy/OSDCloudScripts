3ffb75229fa28a66992a91cb46a5844883480ae2
David Segura <david@segura.org>
2023-09-26 23:07:40 +0200

    Update Start OSDCloud from URL.ps1

--- OSDCloud/Edit-OSDCloudWinPE/Start OSDCloud from URL.ps1
+++ OSDCloud/Edit-OSDCloudWinPE/Start OSDCloud from URL.ps1
@@ -1,2 +1,2 @@
-﻿# Ripped from https://gist.github.com/ajdinaliu/7fc6f8fe4ff785aaffd1ecae6bcb52d3
-Edit-OSDCloudWinPE -StartURL 'https://gist.githubusercontent.com/ajdinaliu/7fc6f8fe4ff785aaffd1ecae6bcb52d3/raw/1f7fe790a329a4d2ed4b6a48656661fed124fff0/OSDCloud%2520ALNs%2520Custom%2520OSDCloud-Menu'
+﻿# Secret Gist https://gist.github.com/OSDeploy/d1d7d12ab512088bcf2ab3d1dfcf1418
+Edit-OSDCloudWinPE -StartURL 'https://gist.githubusercontent.com/OSDeploy/d1d7d12ab512088bcf2ab3d1dfcf1418/raw/c76fd0e85263bc3e1b0d35c9e32e62f4d501fac9/WPNinjaOSDCloud.ps1'
