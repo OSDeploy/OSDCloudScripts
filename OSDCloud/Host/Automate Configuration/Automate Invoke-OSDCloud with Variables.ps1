f2dee83c66653895d02b0426fcb16e2745286602
David Segura <david@segura.org>
2023-09-27 12:01:02 +0200

    23.9.27.1

---
+++ OSDCloud/Host/Automate Configuration/Automate Invoke-OSDCloud with Variables.ps1
@@ -0,0 +1,9 @@
+﻿#Requires -RunAsAdministrator
+
+$Global:MyOSDCloud = @{
+    ImageFileUrl    = 'http://b1.download.windowsupdate.com/c/upgr/2022/09/22621.382.220806-0833.ni_release_svc_refresh_clientconsumer_ret_x64fre_de-de_4b92f7c5f5d5236fe4ba5b3de8f38f847a152db8.esd'
+    OSImageIndex    = 9
+    restartComputer = $false
+}
+
+Invoke-OSDCloud
---
+++ OSDCloud/Host/Automate Configuration/Automate OSDCloudGUI with Variables.ps1
@@ -0,0 +1,71 @@
+﻿#Requires -RunAsAdministrator
+
+$Global:MyOSDCloud = @{
+    BrandName = 'Workplace Ninja 2023'
+    BrandColor = 'RED'
+    OSActivation = 'Retail'
+}
+
+$Automate = @'
+{
+    "BrandName":  "Workplace Ninja 2023",
+    "BrandColor":  "RED",
+    "OSActivation":  "Retail",
+    "OSEdition":  "Pro",
+    "OSLanguage":  "de-de",
+    "OSImageIndex":  9,
+    "OSName":  "Windows 11 22H2 x64",
+    "OSReleaseID":  "22H2",
+    "OSVersion":  "Windows 11",
+    "OSActivationValues":  [
+                                "Retail",
+                                "Volume"
+                            ],
+    "OSEditionValues":  [
+                            "Home",
+                            "Education",
+                            "Enterprise",
+                            "Pro"
+                        ],
+    "OSLanguageValues":  [
+                                "de-de",
+                                "en-us"
+                            ],
+    "OSNameValues":  [
+                            "Windows 11 22H2 x64",
+                            "Windows 10 22H2 x64"
+                        ],
+    "OSReleaseIDValues":  [
+                                "22H2"
+                            ],
+    "OSVersionValues":  [
+                            "Windows 11",
+                            "Windows 10"
+                        ],
+    "ClearDiskConfirm":  false,
+    "restartComputer":  false,
+    "updateDiskDrivers":  false,
+    "updateFirmware":  false,
+    "updateNetworkDrivers":  false,
+    "updateSCSIDrivers":  false
+}
+'@
+
+$AutomateISO = "$(Get-OSDCloudWorkspace)\Media\OSDCloud\Automate"
+if (!(Test-Path $AutomateISO)) {
+    New-Item -Path $AutomateISO -ItemType Directory -Force
+}
+$Automate | Out-File -FilePath "$AutomateISO\Start-OSDCloudGUI.json" -Force
+
+
+$AutomateUSB = "$(Get-OSDCloudWorkspace)\Media\Automate"
+if (!(Test-Path $AutomateUSB)) {
+    New-Item -Path $AutomateUSB -ItemType Directory -Force
+}
+$Automate | Out-File -FilePath "$AutomateUSB\Start-OSDCloudGUI.json" -Force
+
+# Run Edit-OSDCloudWinPE to rebuild
+Edit-OSDCloudWinPE -StartOSDCloudGUI
+
+# Test in a Virtual Machine
+New-OSDCloudVM
