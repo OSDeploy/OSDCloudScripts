54a8b2ec24387e3302afa33810755524b43b9c88
David Segura <david@segura.org>
2023-09-28 13:33:33 +0200

    23.9.28.1

--- WinGet/Import Demo.ps1
+++ WinGet/Import Demo.ps1
@@ -1,4 +1,27 @@
-﻿#WPNinjaS
+﻿<#
+.DESCRIPTION
+The import command of the winget tool imports a JSON file of apps to install.
+The import command combined with the export command allows you to batch install applications on your PC.
+The import command is often used to share your developer environment or build up your PC image with your favorite apps.
+
+.LINK
+https://learn.microsoft.com/en-us/windows/package-manager/winget/import
+
+.NOTES
+Usage
+winget import [-i] <import-file> [<options>]
+
+Arguments
+-i,--import-file            JSON file describing the packages to install.
+
+Options
+--ignore-unavailable        Suppresses errors if the app requested is unavailable.
+--ignore-versions           Ignores versions specified in the JSON file and installs the latest available version.
+--accept-package-agreements Used to accept the license agreement, and avoid the prompt.
+--accept-source-agreements  Used to accept the source license agreement, and avoid the prompt.
+--verbose-logs	            Used to override the logging setting and create a verbose log.
+#>
+#WPNinjaS
 
 $WinGetImport = @'
 {
@@ -80,4 +103,4 @@ $WinGetImport = @'
 '@
 
 $WinGetImport | Out-File $env:TEMP\WinGetImport.json -Encoding ascii -Force
-winget import $env:TEMP\WinGetImport.json
+winget import --import-file $env:TEMP\WinGetImport.json
