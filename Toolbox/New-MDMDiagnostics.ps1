81620fceaef989b5de7b8a47e165d0cdc315b3f9
David Segura <david@segura.org>
2024-02-16 15:15:41 -0600

    Update New-MDMDiagnostics.ps1

--- Toolbox/New-MDMDiagnostics.ps1
+++ Toolbox/New-MDMDiagnostics.ps1
@@ -1,4 +1,5 @@
 #Requires -RunAsAdministrator
 #https://learn.microsoft.com/en-us/windows/client-management/mdm-collect-logs
 
-mdmdiagnosticstool.exe -area "DeviceEnrollment;DeviceProvisioning;AutoPilot;TPM" -cab $env:TEMP\MDMDiagReport-$env:ComputerName.cab
+mdmdiagnosticstool.exe -area 'DeviceEnrollment;DeviceProvisioning;AutoPilot;TPM' -cab "$env:temp\MDMDiagReport-$((Get-Date).ToString('yyyy-MM-dd-HHmmss'))-$env:ComputerName.cab"
+explorer $env:temp
